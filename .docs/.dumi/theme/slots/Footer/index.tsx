16453b9da22f3302681e2feecdcbcc8e58227f70
pl <403802162@qq.com>
2024-08-12 20:21:43 +0800

    更新文档

--- .docs/.dumi/theme/slots/Footer/index.tsx
+++ .docs/.dumi/theme/slots/Footer/index.tsx
@@ -3,31 +3,14 @@
  * @Description: Modify here please
  */
 import { Footer } from "dumi-theme-antd-style";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { getColumns } from "./columns";
 import { baseLink } from "../../../../config";
-import { useLocation } from "umi";
-
-let timer: any = null;
 
 // 全局想往页面注入功能的地方
 
 export default () => {
   const columns = getColumns();
-  const location = useLocation();
-  const v = window.localStorage.getItem("pageIsReload");
-  const isReload = useRef(v);
-
-  useEffect(() => {
-    clearTimeout(timer);
-
-    window.localStorage.setItem("pageIsReload", "0");
-
-    if (location.pathname && isReload.current !== "1") {
-      window.localStorage.setItem("pageIsReload", "1");
-      window.location?.reload();
-    }
-  }, [location.pathname, isReload.current]);
 
   useEffect(() => {
     if (document?.body) {
--- .docs/README.md
+++ .docs/README.md
@@ -5,4 +5,14 @@
 
 # camella-docs
 
+## 📦 安装包
+
+```bash
+// 安装docs文档项目 包依赖最好使用npm i， 我之前用pnpm 感觉导致我的dumi，路由跳转有时候不生效。不知道是不是包导致的，就吧文档部分的包 单独安装了。
+// 切换淘宝源： npm config set registry https://registry.npmmirror.com
+
+npm i
+
+```
+
 [A react library developed with dumi](https://d.umijs.org/guide)
